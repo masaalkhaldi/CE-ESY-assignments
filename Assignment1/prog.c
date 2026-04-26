91ea8ed640560a3c59a1455f4bc1d4e6de990d30
masaalkhaldi <masaalkhaldi685@gmail.com>
2026-04-26 23:15:43 +0300

    prog.c

--- Assignment1/prog.c
+++ Assignment1/prog.c
@@ -1,7 +1,7 @@
+
 #include <stdio.h>
 #include <string.h>
-
-#define SIZE 8
+#define SIZE 10
 
 typedef struct {
     char buffer[SIZE];
@@ -10,7 +10,7 @@ typedef struct {
     int count;
 } CircularBuffer;
 
-void init(CircularBuffer *cb) {
+void initBuffer(CircularBuffer *cb) {
     cb->head = 0;
     cb->tail = 0;
     cb->count = 0;
@@ -26,52 +26,46 @@ int isEmpty(CircularBuffer *cb) {
 
 void writeBuffer(CircularBuffer *cb, char data) {
     if (isFull(cb)) {
-        printf("Buffer Overflow! Cannot write: %c\n", data);
+        printf("Buffer Overflow!\n");
         return;
     }
-
-    cb->buffer[cb->tail] = data;
-    cb->tail = (cb->tail + 1) % SIZE;
+    cb->buffer[cb->head] = data;
+    cb->head = (cb->head + 1) % SIZE;
     cb->count++;
 }
 
 char readBuffer(CircularBuffer *cb) {
     if (isEmpty(cb)) {
-        printf("Buffer Underflow! Buffer is empty.\n");
+        printf("Buffer Underflow!\n");
         return '\0';
     }
-
-    char data = cb->buffer[cb->head];
-    cb->head = (cb->head + 1) % SIZE;
+    char data = cb->buffer[cb->tail];
+    cb->tail = (cb->tail + 1) % SIZE;
     cb->count--;
-
     return data;
 }
 
 int main() {
     CircularBuffer cb;
-    init(&cb);
+    initBuffer(&cb);
 
     char name[50];
-
     printf("Enter your name: ");
     scanf("%s", name);
 
-    strcat(name, "CE-ESY");
+    strcat(name, "YSE-EC");
 
+    // إدخال الاسم في المخزن
     for (int i = 0; i < strlen(name); i++) {
         writeBuffer(&cb, name[i]);
     }
 
+    // قراءة آخر عنصر
     while (!isEmpty(&cb)) {
-        printf("%c", readBuffer(&cb));
+        char c = readBuffer(&cb);
+        printf("%c", c);
     }
 
-    printf("\n");
-
-    if (isEmpty(&cb)) {
-        printf("Buffer is now empty.\n");
-    }
-
+    printf("\nBuffer is now empty.\n");
     return 0;
 }
